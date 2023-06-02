300d6327da46c0de68a0f4d1920e3ddaa1750328
alex.paris <alex.paris@estudiantat.upc.edu>
2023-06-02 17:03:18 +0200

    taulaController

--- demo/src/main/java/com/example/demo/controllers/TaulaController.java
+++ demo/src/main/java/com/example/demo/controllers/TaulaController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controllers;
 
+import com.example.demo.classes.Taula;
 import com.example.demo.services.BlocService;
 import com.example.demo.services.TaulaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaulaController {
     @Autowired
     private TaulaService taulaService;
+
+    @PostMapping("/crearTaula")
+    public long crearTaula(@RequestParam String nom_taula) {
+        Taula t = new Taula(nom_taula);
+        t = taulaService.saveTaula(t);
+        return t.getId();
+    }
 }
